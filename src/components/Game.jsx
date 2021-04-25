7dd78c4fa1e0f179f2d192c3f12eebe18f160b28
arunbhalli <tomarbhalli@gmail.com>
2021-04-25 17:33:48 +0200

    added useEffect methode for computer randome choice

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import {useEffect, useState} from "react";
 
-export const Game = () => {
+export const Game = (score, myChoice, setScore) => {
   const [computer, setComputer] = useState("");
   const [playerWin, setPlayerwin] = useState("");
 
-  const newhouse = () => {
-    const choice = ["rock","paper","scissor"];
-    setComputer(choice[Math.floor(Math.random()*3)])
-  }
-  return (
-    <div>
-    </div>
-  )
-}
-export default Game
+  const newComputerChoice = () => {
+    const choice = ["rock", "paper", "scissor"];
+    setComputer(choice[Math.floor(Math.random() * 3)]);
+  };
+  useEffect(() => {
+    newComputerChoice();
+  }, []);
+
+  return
+  <div className="game">
+ my choice:{myChoice} <br/>
+ Computer Choice:{computer}
+  </div>;
+};
+export default Game;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Menu, Segment } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+import {  Button, Icon } from 'semantic-ui-react'
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
-  )
-}
-
-export default Header
+    <Segment inverted color="blue">
+      <Menu inverted secondary>
+         <Menu.Item
+          name="Game"
+          as={NavLink}
+          to={{pathname: "/game" }}
+        />
+         <Menu.Item
+          name="play"
+          as={NavLink}
+          to={{ pathname: "/play" }}
+        />
+      </Menu>
+    </Segment>
+  );
+};
+export default Header;
