833a4414101cc3c7decbc4768a340deaef8e9af5
arunbhalli <tomarbhalli@gmail.com>
2021-04-28 19:07:28 +0200

    Add test for if user choose paper

--- cypress/integration/SelectingWinner.feature.js
+++ cypress/integration/SelectingWinner.feature.js
@@ -11,16 +11,43 @@ describe("User select  Rock ", () => {
 	});
 
 	it("Computer  choose Scissors and loses", () => {
-		cy.get('[data-cy="Scissor"]').click();
+		cy.get('[data-cy="Rock"]').click();
 		cy.get('[data-cy="my-choice"]').should("contain", "My choice: Rock");
 		cy.get('[data-cy="computer"]').should("contain","Computer choice: Scissors");
 		cy.get('[data-cy="result"]').should("contain", "Congratulation You Won!");
 	});
 
 	it("Computer  choose Paper and wins", () => {
-		cy.get('[data-cy="Paper"]').click();
+		cy.get('[data-cy="Rock"]').click();
 		cy.get('[data-cy="my-choice"]').should("contain",	"My choice: Rock");
 		cy.get('[data-cy="computer"]').should("contain","Computer choice: Paper");
 		cy.get('[data-cy="result"]').should("contain", "Computer Won!");
+	});
+});
+
+describe("User select  Paper ", () => {
+	beforeEach(() => {
+		cy.visit("/")
+	});
+  
+	it("Computer  choose Rock and loses ", () => {
+		cy.get('[data-cy="Paper"]').click();
+		cy.get('[data-cy="my-choice"]').should("contain", "My choice: Paper");
+		cy.get('[data-cy="computer-selection"]').should("contain","Computer choice: Rock");
+		cy.get('[data-cy="result"]').should("contain", "Congratulation You Won!");
+	});
+
+	it("Computer  choose Scissors and Won", () => {
+		cy.get('[data-cy="Paper"]').click();
+		cy.get('[data-cy="my-choice"]').should("contain", "My choice: Paper");
+		cy.get('[data-cy="computer"]').should("contain","Computer choice: Scissors");
+		cy.get('[data-cy="result"]').should("contain", "Computer Won!");
+	});
+
+	it("Computer  choose Paper it is a draw", () => {
+		cy.get('[data-cy="Paper"]').click();
+		cy.get('[data-cy="my-choice"]').should("contain",	"My choice: Paper");
+		cy.get('[data-cy="computer"]').should("contain","Computer choice: Paper");
+		cy.get('[data-cy="result"]').should("contain", "It is a draw!");
 	});
 });
