994a6fef95f3aa0da3f920863cff9a58992e3c33
arunbhalli <tomarbhalli@gmail.com>
2021-04-30 02:03:28 +0200

    adds styling in buttons Button for checking rules Link for playing game

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Button, Container } from "semantic-ui-react";
-import Game from "./Game"
+import { Link } from "react-router-dom";
+import { Button } from "semantic-ui-react";
+import Game from "./Game";
 
 const choices = ["rock", "paper", "scissor"];
 class Play extends React.Component {
@@ -13,16 +14,15 @@ class Play extends React.Component {
   playGame = () => {
     this.setState({
       computer: choices[Math.floor(Math.random() * 2)],
-      winner: ""
+      winner: "",
     });
     this.setState({
       winner: this.selectWinner(),
     });
   };
 
- 
   selectWinner = () => {
-    const { myChoice, computer} = this.state;
+    const { myChoice, computer } = this.state;
     if (myChoice === computer) {
       return "It is a draw";
     } else if (
@@ -41,27 +41,38 @@ class Play extends React.Component {
       winner: "",
     });
   };
-  // debugger 
+  // debugger
   render() {
-    const { myChoice, computer, winner} = this.state
+    const { myChoice, computer, winner } = this.state;
     return (
       <>
+      <Button className="btn"><Link to="/header" className="link">
+          Rules</Link></Button>
         <h1>Welcome To The Rock, Paper & Scissor Game</h1>
-        <div> <h2> Select your  choice</h2>
+        <div>
+          <h2> Select your choice</h2>
           <Button className="button">
-            <div data-cy="rock" onClick={() => this.selectChoice('rock')}>Rock</div>
-          </Button>
-          <Button >
-            <div data-cy="paper"  onClick={() => this.selectChoice("paper")}>Paper</div>
+            <div data-cy="rock" onClick={() => this.selectChoice("rock")}>
+              Rock
+            </div>
           </Button>
           <Button>
-            <div data-cy="scissor" onClick={() => this.selectChoice("scissor")}>Scissor</div>
+            <div data-cy="paper" onClick={() => this.selectChoice("paper")}>
+              Paper
+            </div>
           </Button>
-           <Game datacy="my-choice" choice={myChoice}/>
-           <Game  datacy="computer" choice={computer}/>
-           <h1 data-cy="result"> { winner ? this.selectWinner() : null}</h1>
-           <h1>Computer choice</h1>
-          <Button size='massive' data-cy="play-game" onClick={this.playGame}>Start</Button>
+          <Button>
+            <div data-cy="scissor" onClick={() => this.selectChoice("scissor")}>
+              Scissor
+            </div>
+          </Button>
+          <Game datacy="my-choice" choice={myChoice} />
+          <Game datacy="computer" choice={computer} />
+          <h1 className="res">Computer choice</h1>
+          <h1 data-cy="result" className="res"> {winner ? this.selectWinner() : null}</h1>
+          <Button size="massive" data-cy="play-game" onClick={this.playGame}>
+            Start
+          </Button>
         </div>
       </>
     );
