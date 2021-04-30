d04d1364fc1386b3c639b2fd55894976185f8922
arunbhalli <tomarbhalli@gmail.com>
2021-04-30 02:16:35 +0200

    Correction in game rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ class Header extends React.Component {
       <div className="popup">
         <h1> Rules are very simple for this game </h1>
         <h2> 1 - Rock vs Paper = Paper wins</h2>
-        <h2> 2 - Rock vs Scissor = Scissor wins</h2>
-        <h2> 3 -  Rock  vs  Rock = Game is Tie</h2>
+        <h2> 2 - Rock vs Scissor = Rock wins</h2>
+        <h2> 3 - Paper vs Scissor = Scissor wins</h2>
         <Link to="/play" className="link">
           Click here to play
         </Link>
